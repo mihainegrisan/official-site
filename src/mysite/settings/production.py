bfcb0cb9e99679b17e4529253a65a67eb3307be5
mihai.negrisan <mihainegrisan1@gmail.com>
2019-11-26 10:28:06 +0200

    Increased security

--- src/mysite/settings/production.py
+++ src/mysite/settings/production.py
@@ -4,16 +4,20 @@ DEBUG = False
 ALLOWED_HOSTS = ['ip-address', 'mysite.com']
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME':'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME':'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME':'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME':'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
+
+# SECURITY
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 300  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
